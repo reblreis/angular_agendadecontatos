db6ecc6d37742c920adf108afe48310cae54771b
reblreis <rebreis88@gmail.com>
2023-12-04 15:38:55 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
 
-export const routes: Routes = [];
+
+/*
+    Mapeamento das rotas do projeto para
+    exibir / renderizar componentes
+*/
+export const routes: Routes = [ //export = público
+    {
+        path: 'app/login',
+        component: LoginComponent
+    },
+    {
+        path: 'app/register',
+        component: RegisterComponent
+    },
+    {
+        path: '', /*raiz do projeto */
+        pathMatch: 'full',
+        redirectTo: '/app/login'
+    }
+];
