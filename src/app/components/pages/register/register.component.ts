d020866b1b2f2d946029ea58b2f9e1bf8e213077
reblreis <rebreis88@gmail.com>
2023-12-06 16:51:15 -0300

    Formulários reativos, HttpClient e integração com API (Models, Services e Environments)

--- src/app/components/pages/register/register.component.ts
+++ src/app/components/pages/register/register.component.ts
@@ -1,13 +1,75 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CriarContaService } from '../../../services/criar-conta.service';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { PasswordMatchValidation } from '../../../validations/password-match.validation';
+import { CriarContaRequest } from '../../../models/criar-conta.request';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  //método construtor
+  constructor(
+    private criarContaService: CriarContaService
+  ){}
+
+  //formulário para cadastro de usuário
+  form = new FormGroup({
+    nome: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(100)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    senha: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+    ]),
+    senhaConfirmacao: new FormControl('', [
+      Validators.required
+    ])
+  }, {
+    /* adicionando as validações customizadas do formulário */
+    validators: [PasswordMatchValidation.matchPassword]
+  });
+
+  //função auxiliar oara que possamos verificar se algum
+  //campo do formulário está com erro de validação
+  get f() {
+    return this.form.controls;
+  }
+
+  //função para realizar o submit do formulário
+  submit(): void {
+   
+      //preenchendo os dados da requisição
+      const request : CriarContaRequest = {
+        nome: this.form.value.nome as string,
+        email: this.form.value.email as string,
+        senha: this.form.value.senha as string
+      };
+
+      //executando a camada de serviço para realizar
+      //a chamada para o método POST da API
+      this.criarContaService
+        .post(request)
+        .subscribe({
+          next: (data) => { //capturar a resposta de sucesso
+            console.log(data);
+          },
+          error: (e) => { //capturar a resposta de erro
+            console.log(e);
+          }
+        });
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+/*
+    Valores das variáveis de ambiente
+    em modo local (desenvolvimento)
+*/
+export const environment = {
+    apiContatos: 'http://apicontatoscoti-001-site1.itempurl.com/api'
+};
